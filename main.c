81258359de2107ebd09e82130816f9a7924f2d72
duslerke <43747286+Duslerke@users.noreply.github.com>
2026-03-08 15:33:44 +0000

    Add basic port audio initialisation test code.

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <portaudio.h> // the "Interface" for PortAudio
 
 int main()
 {
-    printf("DSP Tuner Build Successful!\n");
+    PaError err;
+
+    // 1. Initialize the library
+    err = Pa_Initialize();
+    if (err != paNoError)
+    {
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+        return 1;
+    }
+
+    printf("PortAudio initialized successfully!\n");
+
+    // 2. Clean up before exiting
+    Pa_Terminate();
     return 0;
 }
