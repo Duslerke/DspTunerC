0f79d426dcb0c766d83257aa40683150aecb8ddc
duslerke <43747286+Duslerke@users.noreply.github.com>
2026-03-20 20:06:02 +0000

    Define dummy recording callback and open default mic stream.

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <portaudio.h>
 
+const double SAMPLE_RATE = 44100;
+const int FRAMES_PER_BUFFER = 256;
+
+int captureMicInput(
+    const void *paInputBuffer,
+    void *paOutputBuffer,
+    unsigned long framesPerBuffer,
+    const PaStreamCallbackTimeInfo *timeInfo,
+    PaStreamCallbackFlags statusFlags,
+    void *userData)
+{
+    const float *f32Buffer = (const float *)paInputBuffer;
+
+    printf("First sample within the buffer %f\n", f32Buffer[0]);
+
+    return paContinue;
+}
+
 int main()
 {
     PaError err;
@@ -34,6 +52,27 @@ int main()
 
     printf("Name: %s\n", deviceInfo->name);
     printf("Default Sample Rate: %f\n", deviceInfo->defaultSampleRate);
+
+    printf("Opening Default device stream.\n");
+
+    PaStream *stream;
+    err = Pa_OpenDefaultStream(
+        &stream,
+        1,         // mono mic
+        0,         // no output channels
+        paFloat32, // practically infinite dynamic range
+        SAMPLE_RATE,
+        FRAMES_PER_BUFFER,
+        captureMicInput,
+        NULL);
+
+    if (err != paNoError)
+    {
+        fprintf(stderr, "Failed to open the default device stream.");
+        Pa_Terminate();
+        return 1;
+    }
+
 
     Pa_Terminate();
     return 0;
